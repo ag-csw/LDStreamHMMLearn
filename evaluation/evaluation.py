5939bf1f035ee49097adfffdac574398b57fcbcc
Alexandra La Fleur <alexandra.lafleur@fu-berlin.de>
2016-08-11 11:31:31 +0200

    added first scripts with stubs for future implementations

---
+++ evaluation/evaluation.py
@@ -0,0 +1,8 @@
+"""
+This class is for evaluation purposes.
+"""
+
+class Evaluation():
+
+    def evaluate(self):
+        pass
---
+++ util/util.py
@@ -0,0 +1,28 @@
+"""
+This class holds utility functions.
+"""
+class Utility():
+
+    ###################################
+    #   HMM utility functions
+    ###################################
+    @staticmethod
+    def build_hmm():
+        pass
+
+
+
+    ###################################
+    #   Evaluation utility functions
+    ###################################
+    @staticmethod
+    def evaluate_performance():
+        pass
+
+    @staticmethod
+    def evaluate_complexity():
+        pass
+
+    @staticmethod
+    def evaluate_emission_matrices():
+        pass
