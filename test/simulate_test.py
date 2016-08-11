f3fc2319dacd1a10dff1abf6c577549314a37837
greenTara <taraathan@gmail.com>
2016-08-11 14:32:31 +0200

    modify parameter sequences to fit modified pyemma and adjusted tests
    - The learning was changed to take the observed data rather then the
    hidden data.
    - The data size was increased to get better accuracy.
    - The transition matrix was changed to give metastable states relative
    to the time step.
    - The emission matrix was given with independent values, and of size 3x4

--- test/simulate_test.py
+++ test/simulate_test.py
@@ -6,64 +6,70 @@ class MSM_Test():
     ########################################
     # Simulate Functions
     ########################################
-    def test_simulate_MSM (self, transition_matrix, time_steps, num_traj, initial_states):
+    def test_simulate_MSM (self, transition_matrix, time_steps, initial_state):
         model = MSM.MSM(P=transition_matrix)
-        sim = (model.simulate(time_steps=3, num_traj=3, initial_states=initial_states ))
+        sim = (model.simulate(N=3, start=initial_state ))
         return sim
 
-    def MSM_simulate_test_all(self, transition_matrix, time_step, num_traj, initial_states):
-        traj_MSM_num_traj = self.test_simulate_MSM(transition_matrix, time_step, num_traj, None)
+    def MSM_simulate_test_all(self, transition_matrix, time_step, initial_state):
+        traj_MSM_num_traj = self.test_simulate_MSM(transition_matrix, time_step, None)
         print("MSM Trajectory Simulation with num_traj:\n ", traj_MSM_num_traj)
-        traj_MSM_initial_states = self.test_simulate_MSM(transition_matrix, time_step, None, initial_states)
-        print("MSM Trajectory Simulation with initial_states:\n", traj_MSM_initial_states)
-        traj_MSM_both = self.test_simulate_MSM(transition_matrix, time_step, num_traj, initial_states)
+        traj_MSM_initial_state = self.test_simulate_MSM(transition_matrix, time_step, initial_state)
+        print("MSM Trajectory Simulation with initial_state:\n", traj_MSM_initial_state)
+        traj_MSM_both = self.test_simulate_MSM(transition_matrix, time_step, initial_state)
 
-    def test_simulate_HMSM(self, transition_matrix, pobs, time_steps, num_traj, initial_states):
+    def test_simulate_HMSM(self, transition_matrix, pobs, time_steps, initial_state):
         model = MSM.HMSM(P=transition_matrix, pobs=pobs)
-        sim = (model.simulate(time_steps=time_steps, num_traj=num_traj, initial_states=initial_states))
+        sim = (model.simulate(N=time_steps, start=initial_state))
         return sim
-        print("MSM Trajectory Simulation with both num_traj and initial_states:\n", traj_MSM_both)
+        print("MSM Trajectory Simulation with both num_traj and initial_state:\n", traj_MSM_both)
 
-    def HMSM_simulate_test_all(self, transition_matrix, pobs, time_step, num_traj, initial_states):
-        traj_HMSM_num_traj = self.test_simulate_HMSM(transition_matrix, pobs, time_step, num_traj, None)
-        print("HMSM Trajectory Simulation with num_traj:\n ", traj_HMSM_num_traj)
-        traj_HMSM_initial_states = self.test_simulate_HMSM(transition_matrix, pobs, time_step, None, initial_states)
-        print("HMSM Trajectory Simulation with initial_states:\n", traj_HMSM_initial_states)
-        traj_HMSM_both = self.test_simulate_HMSM(transition_matrix, pobs, time_step, num_traj, initial_states)
-        print("HMSM Trajectory Simulation with both num_traj and initial_states:\n", traj_HMSM_both)
+    def HMSM_simulate_test_all(self, transition_matrix, pobs, time_step, initial_state):
+        traj_HMSM_initial_state = self.test_simulate_HMSM(transition_matrix, pobs, time_step, None)
+        print("HMSM Trajectory Simulation without initial_state:\n", traj_HMSM_initial_state)
+        traj_HMSM_initial_state = self.test_simulate_HMSM(transition_matrix, pobs, time_step, initial_state)
+        print("HMSM Trajectory Simulation with initial_state:\n", traj_HMSM_initial_state)
 
     ########################################
     # HMM Model Creation
     ########################################
     def create_HMSM_model_estimator(self, traj, nstates, lag, bayesian=False):
         if bayesian:
-            estimator = MSM.BayesianHMSM(nstates=nstates, lag=lag, show_progress=False)
+            estimator = MSM.BayesianHMSM(nstates=nstates, lag=lag, reversible=False, show_progress=True)
         else:
-            estimator = MSM.MaximumLikelihoodHMSM(nstates=nstates, lag=lag)
+            estimator = MSM.MaximumLikelihoodHMSM(nstates=nstates, lag=lag, reversible=False)
         return estimator.estimate(traj)
 
     def HMSM_test_all(self, traj, nstates, lag):
         MLH_estimator = self.create_HMSM_model_estimator(traj, nstates, lag, False)
         print("Maximum Likelihood Estimator:\n", MLH_estimator)
+        print("Transition Matrix:\n ", MLH_estimator.transition_matrix)
+        print("Emission Matrix:\n ", MLH_estimator.observation_probabilities)
         bayesian_estimator = self.create_HMSM_model_estimator(traj, nstates, lag, True)
         print("Bayesian Estimator:\n", bayesian_estimator)
-
+        print("Transition Matrix:\n ", bayesian_estimator.transition_matrix)
+        print("Emission Matrix:\n ", bayesian_estimator.observation_probabilities)
 ########################################
 # Functionality tests
 ########################################
-transition_matrix = np.array([[0.25, 0.25, 0.5],
-                                  [0.4, 0.2, 0.4],
-                                  [0.15, 0.55, 0.3]])
-pobs = transition_matrix
-initial_states = [0]
+transition_matrix = np.array([[0.9, 0.05, 0.05],
+                                  [0.1, 0.8, 0.1],
+                                  [0.1, 0.1, 0.8]])
+pobs = np.array(
+                                 [
+                                     [0.7, 0.1, 0.1, 0.1],
+                                     [0.1, 0.7, 0.1, 0.1],
+                                     [0.1, 0.1, 0.7, 0.1]
+                                 ])
+initial_state = 0
 
 test = MSM_Test()
 
-test.MSM_simulate_test_all(transition_matrix, 3, 3, initial_states)
+test.MSM_simulate_test_all(transition_matrix, 3, initial_state)
 print("\n")
-test.HMSM_simulate_test_all(transition_matrix, pobs, 3, 3, initial_states)
+test.HMSM_simulate_test_all(transition_matrix, pobs, 3, initial_state)
 
 print("\n\n")
-traj, obs = test.test_simulate_HMSM(transition_matrix, pobs, 100, 1, None)
+traj, obs = test.test_simulate_HMSM(transition_matrix, pobs, 1000, None)
 print(traj)
-test.HMSM_test_all(traj[0], 3, 1)
+test.HMSM_test_all(obs, 3, 1)
