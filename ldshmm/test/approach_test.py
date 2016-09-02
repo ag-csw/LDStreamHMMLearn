40a3d324e1b2f9fd55d0bbc01e5b2ad9de37cbdc
Alex La Fleur <alexandra.lafleur@fu-berlin.de>
2016-09-02 15:25:49 +0200

    made heatmap plotting work to show parameter influence of taumeta and eta on performance and accuracy.

--- ldshmm/test/approach_test.py
+++ ldshmm/test/approach_test.py
@@ -14,21 +14,9 @@ class Approach_Test(TestCase):
         self.nstates = 4
         self.mmf1_0 = MMFamily1(self.nstates)
         self.mm1_0_0 = self.mmf1_0.sample()[0]
-
-        self.taumeta = 3
-        self.mm1_0_0_scaled = self.mm1_0_0.eval(self.taumeta)
-
-        self.nstep = 100 * self.taumeta
-        self.nwindow = 10 * self.nstep
         self.numsteps = 100
-        self.lentraj = self.nwindow + self.numsteps * self.nstep + 1
         self.ntraj = 20
-        self.r = (self.nwindow - self.nstep) / self.nwindow
 
-        self.data1_0_0 = []
-        for i in range(0, self.ntraj):
-            self.data1_0_0.append(self.mm1_0_0_scaled.simulate(self.lentraj))
-        #print(self.data1_0_0)
 
     def estimate_via_sliding_windows(self, data):
         C = count_matrix_coo2_mult(data, lag=1, sliding=False, sparse=False, nstates = self.nstates)
@@ -45,6 +33,117 @@ class Approach_Test(TestCase):
         return C
 
 
+
+    def test_approach_heatmap(self):
+        # initialize timing and error arrays for naive and bayes
+        etimenaive = np.zeros(self.numsteps + 2, dtype=float)
+        etimenaive[0] = 0
+        err = np.zeros(self.numsteps + 1, dtype=float)
+
+        etimebayes = np.zeros(self.numsteps + 2, dtype=float)
+        errbayes = np.zeros(self.numsteps + 1, dtype=float)
+
+        # initialize average timing and error arrays for naive and bayes
+        avg_times_naive = np.zeros((3,3))
+        avg_errs_naive = np.zeros((3,3))
+        avg_times_bayes = np.zeros((3,3))
+        avg_errs_bayes = np.zeros((3,3))
+
+        # specify values for taumeta and eta to iterate over
+        taumeta_values = [2,4,8] # 2,4,8
+        eta_values = [50, 100, 200] # 50,100,200
+
+        for one,taumeta in enumerate(taumeta_values):
+            for two,eta in enumerate(eta_values):
+
+                # Setting taumeta and eta values and recalculate dependent variables for scaling
+                self.taumeta = taumeta
+                self.mm1_0_0_scaled = self.mm1_0_0.eval(self.taumeta)
+                self.nstep = eta * self.taumeta
+                self.nwindow = 10 * self.nstep
+                self.numsteps = 100
+                self.lentraj = self.nwindow + self.numsteps * self.nstep + 1
+                self.ntraj = 20
+                self.r = (self.nwindow - self.nstep) / self.nwindow
+
+                self.data1_0_0 = []
+                for i in range(0, self.ntraj):
+                    self.data1_0_0.append(self.mm1_0_0_scaled.simulate(self.lentraj))
+                dataarray = np.asarray(self.data1_0_0)
+
+
+                # do the timing and error calculation (numsteps+1)- times and calculate the average from these
+                for k in range(0, self.numsteps + 1):
+
+                    ##### naive sliding window approach
+                    data0 = dataarray[:, k * self.nstep: (self.nwindow + k * self.nstep)]
+                    dataslice0 = []
+                    for i in range(0, self.ntraj):
+                        dataslice0.append(data0[i, :])
+                    t0 = process_time()
+                    C0 = self.estimate_via_sliding_windows(dataslice0)  # count matrix for whole window
+                    t1 = process_time()
+                    A0 = _tm(C0)
+                    etimenaive[k + 1] = t1 - t0 + etimenaive[k]
+                    err[k] = np.linalg.norm(A0 - self.mm1_0_0_scaled.trans)
+                    if k==0:
+                        ##### Bayes approach: Calculate C0 separately
+                        data0 = dataarray[:, 0 * self.nstep: (self.nwindow + 0 * self.nstep)]
+                        dataslice0 = []
+                        for i in range(0, self.ntraj):
+                            dataslice0.append(data0[i, :])
+
+                        t0 = process_time()
+                        C_old = self.estimate_via_sliding_windows(dataslice0)
+                        etimebayes[1] = process_time() - t0
+                        errbayes[0] = np.linalg.norm(_tm(C_old) - self.mm1_0_0_scaled.trans)
+
+                    if k>=1:
+                        ##### Bayes approach: Calculate C1 (and any following) usind C0 usind discounting
+                        data1new = dataarray[:, self.nwindow + (k - 1) * self.nstep - 1: (self.nwindow + k * self.nstep)]
+                        dataslice1new = []
+                        for i in range(0, self.ntraj):
+                            dataslice1new.append(data1new[i, :])
+                        t0 = process_time()
+                        C_new = self.estimate_via_sliding_windows(
+                            dataslice1new)  # count matrix for just new transitions
+
+                        weight0 = self.r
+                        weight1 = 1.0
+
+                        C1bayes = weight0 * C_old + weight1 * C_new
+                        C_old = C1bayes
+
+                        t1 = process_time()
+                        etimebayes[k + 1] = t1 - t0 + etimebayes[k]
+                        A1bayes = _tm(C1bayes)
+                        errbayes[k] = np.linalg.norm(A1bayes - self.mm1_0_0_scaled.trans)
+
+                avg_time = sum(etimenaive)/len(etimenaive)
+                avg_err = sum(err)/len(err)
+
+                avg_times_naive[one][two]= avg_time
+                avg_errs_naive[one][two] = avg_err
+
+                avg_time_bayes = sum(etimebayes) / len(etimebayes)
+                avg_err_bayes = sum(errbayes) / len(errbayes)
+
+                avg_times_bayes[one][two] = avg_time_bayes
+                avg_errs_bayes[one][two] = avg_err_bayes
+
+
+
+        print("Naive Performance:", avg_times_naive)
+        print("Bayes Performance:", avg_times_bayes)
+        print("Naive Error:", avg_errs_naive)
+        print("Bayes Error:", avg_errs_bayes)
+
+        # plot the average performances and errors in a heatmap
+        plot_result_heatmap(avg_times_naive, avg_times_bayes, taumeta_values, eta_values, "bayes", "Heatmap Performance")
+        plot_result_heatmap(avg_errs_naive, avg_errs_bayes, taumeta_values, eta_values, "bayes", "Heatmap Error")
+
+
+"""
     def test_approach2(self):
 
         dataarray = np.asarray(self.data1_0_0)
@@ -101,101 +200,46 @@ class Approach_Test(TestCase):
         print("Times (Bayes): ", etimebayes)
         print("Errors (Bayes): ", errbayes)
         plot_result(etimebayes, errbayes, "bayes", "numtraj=20, numsteps=100_8")
-    
-
-    def test_approach3(self):
-        #generation of heatmap
-        etime = np.zeros(self.numsteps + 2, dtype=float)
-        err = np.zeros(self.numsteps + 1, dtype=float)
-        etime[0] = 0
-        avg_times = np.zeros((1,1))
-        avg_errs = np.zeros((1,1))
-        taumeta_values = [2] # 2,4,8    - 2 for testing if heatmap works in general
-        eta_values = [50] # 50,100,200  - 50 for testing if heatmap works in general
-        for one,taumeta in enumerate(taumeta_values):
-            for two,eta in enumerate(eta_values):
-                # Setting taumeta and eta values and recalculate dependent variables
-                self.taumeta = taumeta
-                self.nstep = 100 * self.taumeta
-                self.nwindow = 10 * self.nstep
-                self.numsteps = 100
-                self.lentraj = self.nwindow + self.numsteps * self.nstep + 1
-                self.ntraj = 20
-                self.r = (self.nwindow - self.nstep) / self.nwindow
-
-                self.data1_0_0 = []
-                for i in range(0, self.ntraj):
-                    self.data1_0_0.append(self.mm1_0_0_scaled.simulate(self.lentraj))
-                dataarray = np.asarray(self.data1_0_0)
-
-
-                for k in range(0, self.numsteps + 1):
-                    data0 = dataarray[:, k * self.nstep: (self.nwindow + k * self.nstep)]
-                    dataslice0 = []
-                    for i in range(0, self.ntraj):
-                        dataslice0.append(data0[i, :])
-                    t0 = process_time()
-                    C0 = self.estimate_via_sliding_windows(dataslice0)  # count matrix for whole window
-                    t1 = process_time()
-                    A0 = _tm(C0)
-                    etime[k + 1] = t1 - t0 + etime[k]
-                    err[k] = np.linalg.norm(A0 - self.mm1_0_0_scaled.trans)
-                avg_time = sum(etime)/len(etime)
-                avg_err = sum(err)/len(err)
-                print("Avg Times (Windows): ",avg_time )
-                print("Avg Errors (Windows): ", avg_err )
-
-                print(one,two)
-                avg_times[one][two]= avg_time
-                avg_errs[one][two] = avg_err
-
-        plot_result_heatmap(avg_times, taumeta_values, eta_values, "naive", "Basic Heatmap Performance")
-        #plot_result_heatmap(avg_err, "naive", "Basic Heatmap Error")
-
-        # the following lines are commented out to keep testing simple
 """
-    print("\n############## Bayes #############")
-    etimebayes = np.zeros(self.numsteps + 2, dtype=float)
-    errbayes = np.zeros(self.numsteps + 1, dtype=float)
-
-    weight0 = self.r
-    weight1 = 1.0
-
-    data0 = dataarray[:, 0 * self.nstep: (self.nwindow + 0 * self.nstep)]
-    dataslice0 = []
-    for i in range(0, self.ntraj):
-        dataslice0.append(data0[i, :])
-
-    t0 = process_time()
-    C_old = self.estimate_via_sliding_windows(dataslice0)
-    etimebayes[1] = process_time() - t0
-    errbayes[0] = np.linalg.norm(_tm(C_old) - self.mm1_0_0_scaled.trans)
-
-    for k in range(1, self.numsteps + 1):
-        data1new = dataarray[:, self.nwindow + (k - 1) * self.nstep - 1: (self.nwindow + k * self.nstep)]
-        dataslice1new = []
-        for i in range(0, self.ntraj):
-            dataslice1new.append(data1new[i, :])
-        t0 = process_time()
-        C_new = self.estimate_via_sliding_windows(dataslice1new)  # count matrix for just new transitions
-
-        C1bayes = weight0 * C_old + weight1 * C_new
-        C_old = C1bayes
-        t1 = process_time()
-        etimebayes[k + 1] = t1 - t0 + etimebayes[k]
-        A1bayes = _tm(C1bayes)
-        errbayes[k] = np.linalg.norm(A1bayes - self.mm1_0_0_scaled.trans)
-    print("Times (Bayes): ", etimebayes)
-    print("Errors (Bayes): ", errbayes)
-    plot_result_heatmap(etimebayes, errbayes, "bayes", "numtraj=20, numsteps=100_2")
 """
-def plot_result_heatmap(data, x, y, type, heading):
-    x, y = np.meshgrid(x, y)
-    intensity = np.array(data)
-    plt.pcolormesh(x, y, intensity)
-    plt.colorbar()
-    plt.title(heading)
-    plt.savefig("test.png")
+    def test_plot(self):
+        print("in plot")
+        data = [[0.23465,0.43245,0.26542],
+                [0.97534,0.63123,0.76745],
+                [0.89898, 0.47348,0.12466]]
+        x = [2,4,7]
+        y = [50,100,200]
+        type="try"
+        heading="test"
+        plot_result_heatmap(data,x,y,type,heading)
+"""
+
+
+def plot_result_heatmap(data_naive, data_bayes, x_labels, y_labels, type, heading):
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.pcolor(data_naive, cmap="Reds")
+        plt.title(heading)
+        plt.xticks(np.arange(3), (str(x_label) for x_label in x_labels))
+        plt.yticks(np.arange(3), (str(y_label) for y_label in y_labels))
+        plt.xlabel("taumeta")
+        plt.ylabel("eta")
+        plt.title("Naive Performance")
+        plt.colorbar()
+        plt.tight_layout(2)
+
+        plt.subplot(1,2,2)
+        plt.pcolor(data_bayes, cmap="Reds")
+        plt.title("Bayes Performance")
+        plt.xticks(np.arange(3), (str(x_label) for x_label in x_labels))
+        plt.yticks(np.arange(3), (str(y_label) for y_label in y_labels))
+        plt.xlabel("taumeta")
+        plt.ylabel("eta")
+        plt.colorbar()
+        plt.tight_layout(2)
+
+
+        plt.savefig(heading+".png")
 
 def plot_result(y_axis1_list, y_axis2_list, type, heading):
     """Plotting function for diagram with two y axes
