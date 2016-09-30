532371dc77214421c6cb8949e2b0ca08ff5d46aa
Alex La Fleur <alexandra.lafleur@fu-berlin.de>
2016-09-30 17:42:53 +0200

    added global variables for timescaledisp, timescalemin, statconc, omega to variable holder for upcoming nonstationary evaluation script

--- ldshmm/util/variable_holder.py
+++ ldshmm/util/variable_holder.py
@@ -8,12 +8,21 @@ class Variable_Holder():
     heatmap_size = 3
     min_taumeta = 2
     min_nu = 2
+    min_timescale_min = 1
+    min_timescaledisp = 1
+    min_statconc = 1/64
+    min_omega = 1
 
     mid_taumeta = Utility.get_mid_value(min_taumeta, heatmap_size)
     mid_eta = Utility.get_mid_value(min_eta, heatmap_size)
     mid_scale_win = Utility.get_mid_value(min_scale_win, heatmap_size)
     mid_num_traj = Utility.get_mid_value(min_num_traj, heatmap_size)
     mid_nu = Utility.get_mid_value(min_nu, heatmap_size)
+    mid_timescalemin  = Utility.get_mid_value(min_timescale_min, heatmap_size)
+    mid_timescaledisp = Utility.get_mid_value(min_timescaledisp, heatmap_size)
+    mid_statconc = 1
+    mid_omega = Utility.get_mid_value(min_omega, heatmap_size)
+
 
     product_mid_values = mid_eta * mid_scale_win * mid_num_traj
     numsteps_global = 16 * product_mid_values
